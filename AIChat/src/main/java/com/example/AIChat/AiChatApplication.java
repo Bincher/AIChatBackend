660e4d64969bc1cff7ba09b933be241f231e7182
seongbin <catsb21@gmail.com>
2025-01-09 17:19:46 +0900

    kafka 테스트

--- AIChat/src/main/java/com/example/AIChat/AiChatApplication.java
+++ AIChat/src/main/java/com/example/AIChat/AiChatApplication.java
@@ -10,4 +10,6 @@ public class AiChatApplication {
 		SpringApplication.run(AiChatApplication.class, args);
 	}
 
+	
+
 }
---
+++ AIChat/src/main/java/com/example/AIChat/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.AIChat.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.example.AIChat.common.CustomWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private CustomWebSocketHandler customWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(customWebSocketHandler, "ws/chat").setAllowedOrigins("*");
+    }
+
+}
