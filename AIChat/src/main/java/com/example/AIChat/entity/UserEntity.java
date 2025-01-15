a369b2d0f4b2d3ae748d82d8228e0285e9f71055
seongbin <catsb21@gmail.com>
2025-01-15 15:29:22 +0900

    sql 변경에 따른 entity 코드 수정

--- AIChat/src/main/java/com/example/AIChat/entity/UserEntity.java
+++ AIChat/src/main/java/com/example/AIChat/entity/UserEntity.java
@@ -5,18 +5,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name="user")
+@Table(name="user")
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+    
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     @Column(nullable = false, unique = true)
-    private String username;
+    private String nickname;
 
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = true)
+    private String phoneNumber;
 }
